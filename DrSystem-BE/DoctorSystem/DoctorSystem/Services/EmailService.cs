92c10115289ea151dcee323c899a5dbce29d49a5
Farkas Gergely <f.geri0726@gmail.com>
2022-01-08 22:01:02 +0100

    email account change

--- DrSystem-BE/DoctorSystem/DoctorSystem/Services/EmailService.cs
+++ DrSystem-BE/DoctorSystem/DoctorSystem/Services/EmailService.cs
@@ -16,7 +16,7 @@ namespace DoctorSystem.Services
         public void sendEmail(string to, string content, string _subject)
         {
             var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse("farkas.gergely2002@tanulo.boronkay.hu"));
+            email.From.Add(MailboxAddress.Parse("DoctorSystemapp@gmail.com"));
             email.To.Add(MailboxAddress.Parse(to));
             email.Subject = _subject;
             email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = content };
@@ -25,8 +25,8 @@ namespace DoctorSystem.Services
             var smtp = new SmtpClient();
             smtp.CheckCertificateRevocation = false;
 
-            smtp.Connect("smtp.office365.com", 587, MailKit.Security.SecureSocketOptions.StartTlsWhenAvailable);
-            smtp.Authenticate("farkas.gergely2002@tanulo.boronkay.hu", "YvAzAQu5u");
+            smtp.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTlsWhenAvailable);
+            smtp.Authenticate("DoctorSystemapp@gmail.com", "Aw3Se4Dr5");
             smtp.Send(email);
             smtp.Disconnect(true);
 
