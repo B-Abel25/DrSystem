0cf32cc5ae9fdec9b0dcb58de6acbf8de88a5f40
Farkas Gergely <f.geri0726@gmail.com>
2022-02-22 22:04:28 +0100

    message interface

--- DrSystem-FE/DrSystemapp/src/app/_models/message.ts
+++ DrSystem-FE/DrSystemapp/src/app/_models/message.ts
@@ -1,3 +1,10 @@
-export interface Message{
-    
+export interface Message {
+  id: number;
+  senderId: number;
+  senderUsername: string;
+  recipentId: number;
+  recipentUsername: string;
+  content: string;
+  dateRead?: Date;
+  messageSent: Date;
 }
