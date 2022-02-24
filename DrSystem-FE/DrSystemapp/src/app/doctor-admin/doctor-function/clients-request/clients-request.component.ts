a17ac1058843d46e3c6f8cb9369eacd403ae555d
0525.b.abel@gmail.com <0525.b.abel@gmail.com>
2022-02-24 13:09:10 +0100

    nálam mőködik

--- DrSystem-FE/DrSystemapp/src/app/doctor-admin/doctor-function/clients-request/clients-request.component.ts
+++ DrSystem-FE/DrSystemapp/src/app/doctor-admin/doctor-function/clients-request/clients-request.component.ts
@@ -25,7 +25,7 @@ export class ClientsRequestComponent implements OnInit {
     this.doctor = JSON.parse(localStorage.getItem("doctor"));
     console.log(this.doctor);
     this.loadDoctorClientsRequest();
-
+    console.log("anyád");
   }
 
   loadDoctorClientsRequest() {
