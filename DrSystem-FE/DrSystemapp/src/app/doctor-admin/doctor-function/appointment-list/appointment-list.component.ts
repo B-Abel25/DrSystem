4238cc919d93f287fc8ac033d700f3ad783b3b74
Farkas Gergely <f.geri0726@gmail.com>
2022-03-21 10:10:33 +0100

    appointment and design

--- DrSystem-FE/DrSystemapp/src/app/doctor-admin/doctor-function/appointment-list/appointment-list.component.ts
+++ DrSystem-FE/DrSystemapp/src/app/doctor-admin/doctor-function/appointment-list/appointment-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/core';
 import esLocale from '@fullcalendar/core/locales/hu';
+declare let $: any;
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrls: ['./appointment-list.component.css'],
 })
+
 export class AppointmentListComponent implements OnInit {
+  
   calendarOptions: CalendarOptions = {};
   minTime="10:00:00";
   showModal: boolean;
@@ -24,15 +27,30 @@ export class AppointmentListComponent implements OnInit {
       eventMinHeight: 2,
       allDaySlot: false,
       slotMinTime: this.minTime,
-      events: [
-        { title: 'event 1', date: '2022-03-01T10:00:00+01:00', color: 'red' },
-        { title: 'event 2', date: '2020-06-30' },
-      ],
+     contentHeight:500,
       headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
+      
       },
-
+      // eventClick:function(arg){
+        
+   
+      //   $("#myModal").modal("show");
+      //   $(".modal-title, .eventstarttitle").text("");
+       
+      //   $(".modal-title").text("Foglalás erre a napra :");
+       
+        
+      //   $(".eventstarttitle").text("Hello");
+        
+      // },
+      eventClick:function(arg){
+        alert(arg.event.title)
+        alert(arg.event.start)
+      },
+      events: [
+        { title: 'Igy kaphatom meg az event adatait', start: '2022-03-21T10:00:00+01:00', end:'2022-03-21T10:10:00+01:00', color:'red' },
+        { title: 'event 2', date: '2022-03-21T11:00:00+01:00-11:10:00+01:00', color:'yellow' }
+      ],
       themeSystem: 'bootstrap5',
       slotDuration: '00:10:00',
       editable: false,
