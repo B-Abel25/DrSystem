add5323905d5ed4c33dcb1aff75e5bdbbe5fd29f
0525.b.abel@gmail.com <0525.b.abel@gmail.com>
2022-03-22 19:30:43 +0100

    AppointmentDto renameing

--- DrSystem-BE/DoctorSystem/Dtos/AppointmentDto.cs
+++ DrSystem-BE/DoctorSystem/Dtos/AppointmentDto.cs
@@ -5,17 +5,20 @@ namespace DoctorSystem.Dtos
 {
     public class AppointmentDto
     {
-        public string Name { get; set; }
-        public DateTime DateStart { get; set; }
-        public DateTime DateEnd { get; set; }
+        public string Title { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
         public string Description { get; set; }
+        public string Color { get; set; }
 
         public AppointmentDto(Appointment app)
         {
-            this.Name = app.AppointmentingUser.Name;
-            this.DateStart = app.Date;
-            this.DateEnd = app.Date.AddMinutes(app.Doctor.Duration);
+            this.Title = app.AppointmentingUser.Name;
+            this.Start = app.Date;
+            this.End = app.Date.AddMinutes(app.Doctor.Duration);
             this.Description = app.Description;
+            this.Color = "red"; 
+
         }
 
         public AppointmentDto()
