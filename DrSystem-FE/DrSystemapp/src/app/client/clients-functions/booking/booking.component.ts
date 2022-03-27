bbe6648f1336106aac2e13f01ecd527fbeac3fd6
Farkas Gergely <f.geri0726@gmail.com>
2022-03-27 23:10:45 +0200

    dynamic working

--- DrSystem-FE/DrSystemapp/src/app/client/clients-functions/booking/booking.component.ts
+++ DrSystem-FE/DrSystemapp/src/app/client/clients-functions/booking/booking.component.ts
@@ -18,7 +18,9 @@ import { calendar } from 'ngx-bootstrap/chronos/moment/calendar';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import { Appointment } from 'src/app/_models/appointment';
+import { officeHours } from 'src/app/_models/officeHours';
 import { AppointmentService } from 'src/app/_services/appointment.service';
+import { OfficeHoursService } from 'src/app/_services/office-hours.service';
 
 declare let $: any;
 
@@ -43,6 +45,9 @@ export class BookingComponent implements OnInit {
   slotDuration = '00:10:00';
   currentDate = new Date();
   myDate = Date.now();
+  duration:number;
+  open:string;
+  close:string;
   constructor(
     private location: LocationStrategy,
     private formBuilder: FormBuilder,
@@ -50,6 +55,7 @@ export class BookingComponent implements OnInit {
     private appointmentService: AppointmentService,
     private toastr: ToastrService,
     private modalService: BsModalService,
+    private officeHoursService:OfficeHoursService
   ) {
     history.pushState(null, null, window.location.href);
     this.location.onPopState(() => {
@@ -92,103 +98,13 @@ export class BookingComponent implements OnInit {
 
   ngOnInit() {
     this.initializationForm();
+    this.loadOfficeHours();
+    console.log(this.open);
+    console.log(this.close);
     console.log('ott');
     this.loadClientAppointments();
     console.log('itt');
-    this.calendarOptions = {
-      dateClick: this.handleDateClick.bind(this),
-      weekends: false,
-      initialView: 'timeGridWeek',
-      locale: esLocale,
-      timeZone: 'local',
-      slotEventOverlap: false,
-      //eventMinHeight:2,
-      allDaySlot: false,
-      slotMinTime: this.minTime,
-      eventClick: function (arg) {
-        alert(arg.event.title);
-        alert(arg.event.start);
-      },
-      events: [
-        {
-          title: 'green',
-          start: '2022-03-23T10:40:00+01:00',
-          end: '2022-03-23T10:50:00+01:00',
-          color: 'blue',
-        },
-        {
-          title: 'purple',
-          start: '2022-03-23T10:50:00+01:00',
-          end: '2022-03-23T11:00:00+01:00',
-          color: '#fd7e14',
-        },
-        {
-          title: 'blue',
-          start: '2022-03-23T11:00:00+01:00',
-          end: '2022-03-23T11:10:00+01:00',
-          color: 'blue',
-        },
-        {
-          title: 'light blue',
-          start: '2022-03-23T11:10:00+01:00',
-          end: '2022-03-23T11:20:00+01:00',
-          color: 'green',
-        },
-        {
-          title: 'lightblue',
-          start: '2022-03-23T11:20:00+01:00',
-          end: '2022-03-23T11:30:00+01:00',
-          color: 'blue',
-        },
-        {
-          title: 'lightblue',
-          start: '2022-03-23T11:30:00+01:00',
-          end: '2022-03-23T11:40:00+01:00',
-          color: 'blue',
-        },
-        //------
-        {
-          title: 'darkgreen',
-          start: '2022-03-24T10:40:00+01:00',
-          end: '2022-03-24T10:50:00+01:00',
-          color: 'darkgreen',
-        },
-        {
-          title: 'dark green',
-          start: '2022-03-24T10:50:00+01:00',
-          end: '2022-03-24T11:00:00+01:00',
-          color: 'dark green',
-        },
-        {
-          title: 'green',
-          start: '2022-03-24T11:00:00+01:00',
-          end: '2022-03-24T11:10:00+01:00',
-          color: 'green',
-        },
-        {
-          title: 'light green',
-          start: '2022-03-24T11:10:00+01:00',
-          end: '2022-03-24T11:20:00+01:00',
-          color: 'light green',
-        },
-        {
-          title: 'lightgreen',
-          start: '2022-03-24T11:20:00+01:00',
-          end: '2022-03-24T11:30:00+01:00',
-          color: 'lighgreen',
-        },
-      ],
-      headerToolbar: {},
-
-      validRange: {
-        start: this.myDate,
-      },
-      eventBackgroundColor: '#ffff',
-      slotDuration: this.slotDuration,
-      editable: false,
-      selectable: false,
-      selectMirror: true,
-    };
+    
   }
   //Show Modal with Forn on dayClick Event
   handleDateClick(arg) {
@@ -233,6 +149,51 @@ export class BookingComponent implements OnInit {
         console.log(appointment);
         console.log(this.Events);
         this.calendarOptions.events = appointment;
+        this.loadDuration();
+        
+      });
+  }
+  loadDuration() {
+    this.officeHoursService.getDurationClient().subscribe((durationGet) => {
+      this.duration = durationGet;
+      this.calendarOptions.slotDuration="00:"+this.duration+":00";
+                           
+    });
+  }
+  loadOfficeHours() {
+    this.officeHoursService.getOfficeHoursClient().subscribe((officeHoursGet) => {
+    //this.open=Math.min(officeHoursGet.map(x=>x.open));
+       this.open=officeHoursGet.sort((one, two) => (one.open < two.open ? -1 : 1))[0].open;
+       this.close=officeHoursGet.sort((one, two) => (one.close > two.close ? -1 : 1))[0].close;                    
+    console.log(this.open);
+    console.log(this.close);
+    this.calendarOptions = {
+      dateClick: this.handleDateClick.bind(this),
+      weekends: false,
+      initialView: 'timeGridWeek',
+      locale: esLocale,
+      timeZone: 'local',
+      slotEventOverlap: false,
+      //eventMinHeight:2,
+      allDaySlot: false,
+      slotMinTime: this.open+":00",
+      slotMaxTime:this.close+":00",
+      eventClick: function (arg) {
+        alert(arg.event.title);
+        alert(arg.event.start);
+      },
+     
+      headerToolbar: {},
+
+      validRange: {
+        start: this.myDate,
+      },
+      eventBackgroundColor: '#ffff',
+      slotDuration: this.slotDuration,
+      editable: false,
+      selectable: false,
+      selectMirror: true,
+    };
       });
   }
 }
