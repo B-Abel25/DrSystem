f0908a230b84c701fa3b6dce136eea35e9c07172
0525.b.abel@gmail.com <0525.b.abel@gmail.com>
2022-04-10 00:32:10 +0200

    doctor transfer endpoint

---
+++ DrSystem-BE/DoctorSystem/Dtos/TransferDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DoctorSystem.Dtos
+{
+    public class TransferDto
+    {
+        [Required]
+        public string MedNumber { get; set; }
+        [Required]
+        public string NewDoctorSealNumber { get; set; }
+    }
+}
